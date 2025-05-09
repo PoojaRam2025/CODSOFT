daff613aaf9a5bff08932b3451cf1d8c582a6925
pooja-work <shalinicse23@gmail.com>
2025-05-09 20:39:40 +0530

    Update task2_calculator.py

--- task2_calculator.py
+++ task2_calculator.py
@@ -1,1 +1,23 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
+print("Choose operation: +, -, *, /")
+op = input("Operation: ")
+
+if op == '+':
+    result = num1 + num2
+    print("Result:", result)
+elif op == '-':
+    result = num1 - num2
+    print("Result:", result)
+elif op == '*':
+    result = num1 * num2
+    print("Result:", result)
+elif op == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Cannot divide by zero!")
+else:
+    print("Invalid operator")
